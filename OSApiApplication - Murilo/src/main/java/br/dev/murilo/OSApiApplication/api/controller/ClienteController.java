b0991ce117ba78d899dde238a390915e754de073
Murilo Torres <155643928+MuriloHsTorres@users.noreply.github.com>
2025-05-23 10:06:01 -0300

    Add files via upload

---
+++ OSApiApplication - Murilo/src/main/java/br/dev/murilo/OSApiApplication/api/controller/ClienteController.java
@@ -0,0 +1,88 @@
+package br.dev.murilo.OSApiApplication.api.controller;
+
+import br.dev.murilo.OSApiApplication.domain.model.Cliente;
+import br.dev.murilo.OSApiApplication.domain.repository.ClienteRepository;
+import br.dev.murilo.OSApiApplication.domain.service.ClienteService;
+import jakarta.validation.Valid;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author sesidevb
+ */
+
+@RestController
+public class ClienteController {
+    
+    @Autowired
+    private ClienteService clienteService;
+
+    @Autowired
+    private ClienteRepository clienteRepository;
+    
+    @GetMapping("/clientes")
+    public List<Cliente> listas(){
+        return clienteRepository.findAll();
+    }
+    
+    @GetMapping("/clientes/{clienteID}")
+    public ResponseEntity<Cliente> buscar (@PathVariable Long clienteID){
+        Optional<Cliente> cliente = clienteRepository.findById(clienteID);
+
+        if (cliente.isPresent()){
+            return ResponseEntity.ok(cliente.get());
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PostMapping("/clientes")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Cliente adicionar(@Valid @RequestBody Cliente cliente){
+
+        return clienteService.salvar(cliente);
+        
+    }
+    
+    //fim da dase 4 - alterado metodo Post e metodo Put
+
+    @PutMapping ("/clientes/{clienteID}")
+    public ResponseEntity<Cliente> atualizar (@Valid @PathVariable Long clienteID,
+                                              @RequestBody Cliente cliente){
+        
+        if (!clienteRepository.existsById(clienteID)){
+            return ResponseEntity.notFound().build();
+        }
+            cliente.setId(clienteID);
+            cliente = clienteService.salvar(cliente);
+            return ResponseEntity.ok(cliente);
+        }
+
+    @DeleteMapping ("/clientes/{clienteID}")
+    public ResponseEntity<Void> excluir (@PathVariable long clienteID){
+        if (!clienteRepository.existsById(clienteID)){
+            return ResponseEntity.notFound().build();
+        }
+        clienteService.excluir(clienteID);
+        return ResponseEntity.noContent().build();
+    }
+//fim da fase 3
+}
---
+++ OSApiApplication - Murilo/src/main/java/br/dev/murilo/OSApiApplication/domain/model/Cliente.java
@@ -0,0 +1,108 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.dev.murilo.OSApiApplication.domain.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+/**
+ *
+ * @author sesidevb
+ */
+ @Entity
+public class Cliente {
+     
+     @Id
+     @GeneratedValue(strategy = GenerationType.IDENTITY)
+     private long id;
+     
+    @NotBlank
+    @Size(max = 60)
+    private String nome;
+    
+    @NotBlank
+    @Email
+    @Size(max = 255)
+    private String email;
+    
+    @NotBlank
+    @Size(max = 20)
+    @Column(name = "telefone") 
+    private String fone;
+    
+    public Cliente() {}
+
+    public Cliente(long id, String nome, String email, String fone) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.fone = fone;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFone() {
+        return fone;
+    }
+
+    public void setFone(String fone) {
+        this.fone = fone;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 59 * hash + (int) (this.id ^ (this.id >>> 32));
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Cliente other = (Cliente) obj;
+        return this.id == other.id;
+    }
+    
+    
+}
+
+//alt + insert
+
